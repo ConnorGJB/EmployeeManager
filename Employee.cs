b69669bf8dec1434364febe2aeeca7ab3bf3e708
SirGrimm <155329397+ConnorGJB@users.noreply.github.com>
2024-09-28 14:57:56 +0100

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Prac1
+{
+    internal class Employee
+    {
+        public string EmployeeFullName { get; set; }
+        public string EmployeeID { get; set; }
+        public double HoursWorked { get; set; }
+        public double HourlyRate { get; set; }
+
+        public Employee(string EmplloyeeFullName, string EmployeeID, double HoursWorked, double HourlyRate = 9.5)
+        {
+            this.EmployeeFullName = EmplloyeeFullName;
+            this.EmployeeID = EmployeeID;
+            this.HoursWorked = HoursWorked;
+            this.HourlyRate = HourlyRate;
+        }
+
+        public override string ToString()
+        {
+            return $"{EmployeeFullName}({EmployeeID})";
+        }
+        public double TotalWage()
+        {
+            return HoursWorked * HourlyRate;
+        }
+        public static bool IsValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name) && name.Length >= 1 && name.Length <= 50;
+        }
+        public static bool IsValidID(string id)
+        {
+            return Regex.IsMatch(id, @"^[A-Za-z]\d{2}$");
+        }
+        public static bool HourValid(double hoursWorked)
+        {
+            return hoursWorked >= 1 && hoursWorked <= 100;
+        }
+
+        public static string GetValidName()
+        {
+            string name;
+            do
+            {
+                Console.Write("Enter your name: (must be 1-50 characters): ");
+                name = Console.ReadLine();
+
+                if (!Employee.IsValidName(name))
+                {
+                    Console.WriteLine("Error invalid name");
+                }
+            } while (!Employee.IsValidName(name));
+            return name;
+
+        }
+
+        public static string GetValidID()
+        {
+            string Id;
+            do
+            {
+                Console.Write("Enter Employee ID: (Must be 1 Letter and 2 numbers): ");
+                Id = Console.ReadLine();
+                if (!Employee.IsValidID(Id))
+                {
+                    Console.WriteLine("Error Invalid ID");
+                }
+            } while (!Employee.IsValidID(Id));
+            return Id;
+        }
+
+        public static double GetHourValid()
+        {
+            double hours;
+            do
+            {
+                Console.Write("Enter total worked hours: ");
+                bool AcceptedNumber = double.TryParse(Console.ReadLine(), out hours);
+                if (!AcceptedNumber || !Employee.HourValid(hours)) ;
+                {
+                    Console.WriteLine("Error: invalid number entered");
+                }
+            } while (!Employee.HourValid(hours));
+            return hours;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿
+using Prac1;
+using System.Formats.Tar;
+using System.Runtime.CompilerServices;
+
+List<Employee> EmployeeList = new List<Employee>();
+//static void main(string[] args) {
+
+var Worker1 = new Employee("Koby Rein", "A45", 35);
+var Worker2 = new Employee("Franz Reid", "B47", 39);
+var Worker3 = new Employee("John Green", "C98", 32);
+
+
+EmployeeList.Add(Worker1);
+EmployeeList.Add((Employee)Worker2);
+EmployeeList.Add((Employee)Worker3);
+
+/*Console.WriteLine(Emp.ToString);
+GetValidName();
+GetValidID();
+GetHourValid();
+Emp.TotalWage();*/
+
+bool Prophet = true;
+
+while (Prophet)
+{
+    Console.WriteLine("Hello, welcome to your employee manager");
+    Console.WriteLine("Please select which option you want:");
+    Console.WriteLine("1: Add a new employee");
+    Console.WriteLine("2: Delete an employee's details");
+    Console.WriteLine("3: Display current saved employees)");
+    Console.WriteLine("4: Quit");
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            addnewEmployee();
+            break;
+        case "2":
+            removeEmployee();
+            break;
+        case "3":
+            displaySavedEmployees();
+            break;
+        case "4":
+            Prophet = false;
+            break;
+        default:
+            Console.WriteLine("Error incorrect entry");
+            break;
+
+
+    }
+}
+
+static void addnewEmployee()
+{
+    string newEmployeeName = Employee.GetValidName();
+    string newEmployeeId = Employee.GetValidID();
+    double newEmployeeHours = Employee.GetHourValid();
+
+    var NewEmployee = new Employee(newEmployeeName, newEmployeeId, newEmployeeHours);
+
+    EmployeeList.Add(NewEmployee);
+
+    Console.WriteLine($"Employee added: {NewEmployee.ToString}");
+}
+static void removeEmployee()
+{
+    displaySavedEmployees();
+    if (EmployeeList.Count == 0)
+    {
+        Console.WriteLine("No Employees found within list");
+        return;
+    }
+    int selected;
+    Console.WriteLine($"Enter number allocated to employee you wish to delete 1 to {EmployeeList.Count}");
+    while (!int.TryParse(Console.ReadLine(), out selected) || selected < 1 || selected > EmployeeList.Count) ;
+    {
+        Console.WriteLine($"Error, invalid input please choose from 1 to {EmployeeList.Count}");
+    }
+    int index = selected;
+    Employee employToDel = EmployeeList[index];
+    EmployeeList.RemoveAt(index);
+
+    Console.WriteLine($"Employee {employToDel.EmployeeFullName}{employToDel.EmployeeID} has been removed");
+}
+
+
+
+
+static void displaySavedEmployees()
+{
+    Console.WriteLine("Current saved lsit of employees: ");
+    int index = 1;
+    foreach (Employee employee in EmployeeList)
+    {
+        Console.WriteLine($"{index}: {employee.ToString()}");
+        Console.WriteLine($"Wage is: ${employee.TotalWage()}");
+        index++;
+    }
+
+}
+
+
+
